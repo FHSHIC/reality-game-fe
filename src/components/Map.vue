8564eaa8aede0296004cf7817578e48424df5662
10704 <11035092@fhsh.tp.edu.tw>
2023-02-03 23:56:07 +0800

    feat: 🎸 新增指引地圖頁面元件

---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+  <form
+    class="flex h-full w-full flex-col items-center justify-center gap-4 px-4 py-6"
+    @submit.prevent="mapMethod"
+  >
+    <label for="where" class="">
+      <p class="border-10"></p>
+    </label>
+    <label for="" class="">
+      <p class="border-10"></p>
+    </label>
+    <input
+      type="submit"
+      value="開始"
+      class="w-full bg-green-600 py-2 text-lg text-gray-100"
+    />
+  </form>
+</template>
+<script setup>
+import { reactive } from "vue";
+
+const map = {};
+
+function mapMethod() {
+  console.log(map);
+}
+</script>
+<style></style>
