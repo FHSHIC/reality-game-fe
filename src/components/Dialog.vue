00060521ce7c194061e44454a43e084314e92553
cxphoenix <jate9229@gmail.com>
2023-02-03 23:40:24 +0800

    feat: 🎸 新增對話框的 component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative mx-auto w-screen py-4 px-6">
+    <div class="max-h-36 overflow-auto border-2 border-black p-3 pt-7">
+      <p class="text-sm">
+        {{ props.speakContent }}
+      </p>
+    </div>
+    <div class="absolute top-0 left-8 border-2 border-black bg-white p-2">
+      <p class="text-sm">
+        {{ props.speakName }}
+      </p>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["speakName", "speakContent"]);
+</script>
