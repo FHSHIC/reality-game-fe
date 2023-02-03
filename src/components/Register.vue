2cd07feeb96ba642bdf7dbc4c838acae9bc72867
10704 <11035092@fhsh.tp.edu.tw>
2023-02-03 23:54:55 +0800

    feat: 🎸 新增註冊表單元件

---
+++ src/components/Register.vue
@@ -0,0 +1,68 @@
+<template>
+  <form
+    class="flex h-full w-full flex-col items-center justify-center gap-4 px-4 py-6"
+    @submit.prevent="registerMethod"
+  >
+    <label for="account-number" class="">
+      <p class="">帳號</p>
+      <input
+        v-model.trim="register.accountNumber"
+        type="text"
+        name=""
+        id="account-number"
+        class="border-2"
+      />
+    </label>
+
+    <label for="email" class="">
+      <p class="">Email</p>
+      <input
+        v-model.trim="register.email"
+        type="email"
+        name=""
+        id="email"
+        class="border-2"
+      />
+    </label>
+    <label for="password" class="">
+      <p class="">密碼</p>
+      <input
+        v-model="register.password"
+        type="password"
+        name=""
+        id="password"
+        class="border-2"
+      />
+    </label>
+    <label for="check-password" class="">
+      <p class="">確認密碼</p>
+      <input
+        v-model="register.checkPassword"
+        type="password"
+        name=""
+        id="check-password"
+        class="border-2"
+      />
+    </label>
+    <input
+      type="submit"
+      value="註冊"
+      class="w-full bg-green-600 py-2 text-lg text-gray-100"
+    />
+  </form>
+</template>
+<script setup>
+import { reactive } from "vue";
+
+const register = {
+  accountNumber: "",
+  email: "",
+  password: "",
+  checkPassword: "",
+};
+
+function registerMethod() {
+  console.log(register);
+}
+</script>
+<style></style>
