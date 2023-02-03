9052e6ac85c277f8218db287d2327dd17be1cd1d
cxphoenix <jate9229@gmail.com>
2023-02-04 00:02:24 +0800

    feat: 🎸 更新提示 Bar 元件

---
+++ src/components/HintBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <input type="text" class="overflow-x-auto p-2" :value="props.hintContent" />
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["hintContent"]);
+</script>
