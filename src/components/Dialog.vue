cd754a05e503d6ddbc44c083b0059de00c37a387
白巧克力 <anniepai@baiqiaokelideMacBook-Air.local>
2023-02-16 22:35:27 +0800

    feat: 🎸 新增文字顏色的 props

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,25 +1,36 @@
+<script setup>
+import { defineProps } from "vue";
+import dialog_content_bg from "../assets/dialog_content_bg.png";
+
+const props = defineProps([
+  "speakName",
+  "speakContent",
+  "nameTextColor",
+  "contentTextColor",
+]);
+</script>
+
 <template>
   <div class="relative mx-auto w-screen py-4 px-6">
     <div
       class="dialog-content-bg relative max-h-32 overflow-auto bg-cover bg-center bg-no-repeat p-3 pt-5"
     >
-      <p class="h-24 overflow-auto pb-2 text-sm">
+      <p
+        class="h-24 overflow-auto pb-2 text-sm"
+        :style="`color: ${contentTextColor}`"
+      >
         {{ props.speakContent }}
       </p>
     </div>
     <div
       class="dialog-name-bg absolute -top-6 left-5 bg-cover bg-center bg-no-repeat p-8"
     >
-      <p class="text-sm">{{ props.speakName }}</p>
+      <p class="text-sm" :style="`color: ${nameTextColor}`">
+        {{ props.speakName }}
+      </p>
     </div>
   </div>
 </template>
-<script setup>
-import { defineProps } from "vue";
-import dialog_content_bg from "../assets/dialog_content_bg.png";
-
-const props = defineProps(["speakName", "speakContent"]);
-</script>
 
 <style>
 .dialog-content-bg {
