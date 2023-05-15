b6bfc785eab8a36f7de96a5d905abf89344e564e
CXPhoenix <jate9229@gmail.com>
2023-05-16 04:56:58 +0800

    feat: 🎸 將會用到的 state 封裝

--- src/utils/status.js
+++ src/utils/status.js
@@ -1,1 +1,29 @@
 import { reacitve } from "vue";
+
+export const userStatus = reacitve({
+  username: "",
+  accessToken: "",
+  gameState: {
+    active: false,
+    teamId: "",
+  },
+  gameHistory: [],
+});
+
+export const teamStatus = reacitve({
+  teamName: "",
+  teamMember: {
+    email: "username",
+  },
+  teamState: {
+    isActive: false,
+    level: 1,
+    startTimestamp: 0,
+    isFinalLevel: false,
+  },
+});
+
+export const userDialogStatus = reacitve({
+  nowDialogSeq: 0,
+  thisLevelDialogLen: 0,
+});
